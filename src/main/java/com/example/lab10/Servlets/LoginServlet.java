c969ca3137c47c76947681d3f555985933bc3045
Carlos1507 <ayala.carlos@pucp.edu.pe>
2022-06-28 00:37:20 -0500

    login

--- src/main/java/com/example/lab10/Servlets/LoginServlet.java
+++ src/main/java/com/example/lab10/Servlets/LoginServlet.java
@@ -5,11 +5,12 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", value = "/LoginServlet")
+@WebServlet(name = "LoginServlet", value = "")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        RequestDispatcher view = request.getRequestDispatcher("iniciaSesion.jsp");
+        view.forward(request, response);
     }
 
     @Override
