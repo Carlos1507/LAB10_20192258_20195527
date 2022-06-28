6d086fd5d18c28bd6592f4e124ca9a3ab2a71407
Carlos1507 <ayala.carlos@pucp.edu.pe>
2022-06-27 23:09:52 -0500

    Beans y Daos

--- src/main/java/com/example/lab10/Daos/BaseDao.java
+++ src/main/java/com/example/lab10/Daos/BaseDao.java
@@ -1,4 +1,20 @@
 package com.example.lab10.Daos;
 
-public class BaseDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class BaseDao {
+    public Connection getConnection () throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        String user = "root";
+        String pass = "root";
+        String url = "jdbc:mysql://localhost:3306/centro1";
+
+        return DriverManager.getConnection(url,user,pass);
+    }
 }
